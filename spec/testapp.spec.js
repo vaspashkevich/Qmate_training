dfc3f96f3de19841552f90f120255888cb4e45ea
Vasili Pashkevich <vasili.pashkevich@LeverX.local>
2023-08-23 01:56:39 +0300

    Executable version

---
+++ spec/testapp.spec.js
@@ -0,0 +1,7 @@
+describe("Order creation", async function () {
+
+  it("Step 01: Navigate to the application", async function () {
+await ui5.navigation.navigateToApplication("categories")
+  
+  });
+});
